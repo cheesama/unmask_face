f03a35e80b327a5764d7935d16aef2f132ed77f2
chee_mac <chee_mac@chee-macui-Macmini.local>
2021-07-26 12:11:01 +0900

    modify pix2pix transform logic

--- frontend/unmasking_face_app/src/App.js
+++ frontend/unmasking_face_app/src/App.js
@@ -1,17 +1,91 @@
-import React, { useState } from 'react';
-import { StatusBar, Image } from 'react-native';
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import { ThemeProvider } from 'styled-components/native';
-import { theme } from './theme';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
-const App = () => {
+let camera: Camera;
+
+export default function App() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+
+    if (hasPermission === false) {
+        return <Text>Please enable the camera permission in the app's settings</Text>;
+    }    
+
     return (
-        <ThemeProvider theme={theme}>
-            <StatusBar barStyle="dark-content" />
-        </ThemeProvider>
+        <View style={styles.container}>
+            <Camera 
+                sytle={styles.camera} 
+                type={type}
+                ref={(r) => {
+                    camera = r
+                }}
+            >
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity 
+                        style={styles.button}
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back
+                            );
+                        }}
+                    >
+                        
+                        <Text style={styles.text}> Flip </Text>
+                        <Text 
+                            style={styles.text_right}
+                            onPress={async () => {
+                                if(!camera) return;
+                                const photo = await camera.takePictureAsync();
+                                console.log(photo);
+                            }}
+                        >
+                            Shot 
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </Camera>
+        </View>
     );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    camera: {
+      flex: 1,
+    },
+    buttonContainer: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      flexDirection: 'row',
+      margin: 20,
+    },
+    button: {
+      flex: 0.1,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 40,
+      color: 'black',
+    },
+    text_right: {
+        fontSize: 60,
+        color: 'black',
+        position: 'absolute',
+        right: -800,
+    }
+});
